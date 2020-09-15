32e9e9a91663e2dec4b960cb3eac06bea69028fa
synzen <michael994664@gmail.com>
2020-09-14 22:51:44 -0400

    Add _id and addedAt to DeliveryRecord

--- src/entities/DeliveryRecord.ts
+++ src/entities/DeliveryRecord.ts
@@ -1,11 +1,15 @@
-import { Entity, Index, Property } from "@mikro-orm/core";
+import { Entity, Index, PrimaryKey, Property } from "@mikro-orm/core";
 import Payload from "../utils/Payload";
+import { ObjectId } from 'mongodb'
 
 @Entity()
 @Index({
   properties: ['channel']
 })
 class DeliveryRecord {
+
+  @PrimaryKey()
+  _id!: ObjectId;
 
   @Property()
   articleID: string;
@@ -21,6 +25,9 @@ class DeliveryRecord {
 
   @Property({nullable: true})
   comment?: string;
+
+  @Property()
+  addedAt = new Date()
 
   constructor(payload: Payload, delivered: boolean) {
     const { article, feed } = payload.data
