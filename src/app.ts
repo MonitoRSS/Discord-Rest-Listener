d75a35a2b00d49e1a14f929cf293199b3cf285cc
synzen <michael994664@gmail.com>
2020-09-14 22:52:01 -0400

    Move setup to different file with mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,17 @@
-import { Pull } from 'zeromq'
-import config from './utils/config'
 import log from './utils/log'
-import { delayQueueBy, enqueue, enqueueOldPayloads, validatePayload } from './services/Queue'
-import RedisCache from './services/RedisCache'
+import { delayQueueBy, enqueue, validatePayload } from './services/Queue'
 import { DiscordRESTHandler } from './services/DiscordRequests'
+import setup from './utils/setup'
 
-const redisCache = new RedisCache(config.redis, config.redisPrefix)
-
-async function createConsumer () {
-  const sock = new Pull()
-  await sock.connect(config.bindingAddress)
-  return sock
-}
-
-async function connectToRedis () {
-  return new Promise((resolve) => {
-    redisCache.on('ready', resolve)
-  })
-}
-
-log.info('Connecting to Redis')
-
-// Connect to redis
-connectToRedis().then(() => {
-  log.info('Redis connection established')
-  // Handle old payloads
-  return enqueueOldPayloads(redisCache)
-}).then(() => {
-  // Start accepting new payloads
-  return createConsumer()
-}).then(async (sock) => {
+setup().then(async ({redisCache, sock}) => {
   // Handle incoming payloads
-  log.info(`Worker connected to ${config.bindingAddress}`)
   for await (const [msg] of sock) {
     const data = JSON.parse(msg.toString())
     if (validatePayload(data)) {
       enqueue(data, redisCache)
     }
   }
-})
-.catch((err) => {
+}).catch(err => {
   log.error(err)
 })
 
---
+++ src/utils/setup.ts
@@ -0,0 +1,48 @@
+import { MikroORM } from "@mikro-orm/core"
+import { Pull } from "zeromq"
+import DeliveryRecord from "../entities/DeliveryRecord"
+import { enqueueOldPayloads } from "../services/Queue"
+import RedisCache from "../services/RedisCache"
+import config from "./config"
+import log from "./log"
+
+async function createConsumer () {
+  const sock = new Pull()
+  await sock.connect(config.bindingAddress)
+  return sock
+}
+
+async function connectToRedis (): Promise<RedisCache> {
+  const redisCache = new RedisCache(config.redis, config.redisPrefix)
+  if (redisCache.client.connected) {
+    return redisCache
+  }
+  return new Promise((resolve) => {
+    redisCache.on('ready', () => resolve(redisCache))
+  })
+}
+
+async function setup () {
+  log.info('Connecting to Mongo')
+  const orm = await MikroORM.init({
+    entities: [DeliveryRecord],
+    type: 'mongo',
+    clientUrl: config.databaseURI
+  })
+  log.info('Connected to Mongo, connecting to Redis')
+  // Connect to redis
+  const redisCache = await connectToRedis() 
+  log.info('Redis connected, enqueuing old payloads')
+  // Handle old payloads
+  await enqueueOldPayloads(redisCache)
+  // Start accepting new payloads
+  const sock = await createConsumer()
+  log.info(`Worker connected to ${config.bindingAddress}`)
+  return {
+    orm,
+    sock,
+    redisCache
+  }
+}
+
+export default setup
