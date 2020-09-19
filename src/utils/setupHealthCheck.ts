2ce53ded8c793d24acd8988866b2d01279186f46
synzen <michael994664@gmail.com>
2020-09-19 12:03:16 -0400

    Log health check hits

--- src/utils/setupHealthCheck.ts
+++ src/utils/setupHealthCheck.ts
@@ -1,17 +1,24 @@
 import express from 'express'
 import config from './config'
-
+import { URL } from 'url'
+import log from './log'
+import expressWinston from 'express-winston'
 /**
  * Create a single HTTP API route /rest for a health check
  */
 function setupHealthCheck () {
   const app = express()
 
+  app.use(expressWinston.logger({
+    winstonInstance: log
+  }))
+
   app.get('/health', (req, res) => {
     res.status(200).end()
   })
-  const bindingAddressParts = config.bindingAddress.split(':')
-  const port = Number(bindingAddressParts[bindingAddressParts.length - 1]) + 1
+
+  const address = new URL(config.bindingAddress)
+  const port = Number(address.port) + 1
   app.listen(port)
   return port
 }
