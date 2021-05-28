27bcea610fcbd9ca759b3f964484aed2d0ee9ae0
synzen <michael994664@gmail.com>
2021-05-27 20:35:43 -0400

    Delete

--- src/app.ts
+++ src/app.ts
@@ -31,12 +31,6 @@ setup().then((initializedData) => {
     maxRequestsPerSecond: 25
   }, 9999)
 
-  producer.queue.pause(true).then(() => {
-    producer.queue.resume(false)
-    producer.queue.resume(true)
-  })
-  
-
   producer.queue.on('completed', async (job, result: JobResponse<Record<string, unknown>>) => {
     log.debug('Job completed', result)
     // This was a feed article
