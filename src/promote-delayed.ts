1d68bcf4172805049ced19ae2ec80589fb8b8441
gintil <searcher64@gmail.com>
2022-01-07 00:24:38 -0500

    Make script more dynamic using env vars

--- src/promote-delayed.ts
+++ src/promote-delayed.ts
@@ -4,10 +4,13 @@ import setup from './utils/setup'
 import { RESTProducer } from '@synzen/discord-rest'
 import config from './utils/config'
 
+const stages = process.env.STAGES?.split(',') || ['delayed']
+const count = Number(process.env.COUNT || 100)
+
 setup(false).then(async () => {
   const producer = new RESTProducer(config.redis)
   log.info('Ready')
-  const jobs = await producer['queue'].getJobs(['delayed'], 0, 500);
+  const jobs = await producer['queue'].getJobs(stages, 0, count);
   console.log(`Found ${jobs.length}`)
   // @ts-ignore
   const results = await Promise.allSettled(jobs.map(async (job) => job.promote()))
