13d34c5eb65540081f2fbf7d7fcb0fc3553ecc57
gintil <searcher64@gmail.com>
2022-01-27 07:39:42 -0500

    Update bull-board

--- src/web.ts
+++ src/web.ts
@@ -1,21 +1,30 @@
 import Queue from 'bull';
-import { BullAdapter, setQueues, router } from 'bull-board'
+import { BullAdapter } from '@bull-board/api/bullAdapter'
+import { ExpressAdapter } from '@bull-board/express'
+import { createBullBoard } from '@bull-board/api'
 import {
     REDIS_QUEUE_NAME
 } from "@synzen/discord-rest"
 import express from 'express'
 import config from "./utils/config"
-const app = express()
 
 const queue = new Queue(REDIS_QUEUE_NAME, {
     redis: config.redis
 })
 
-app.use(router)
+const serverAdapter = new ExpressAdapter();
 
-setQueues([
-  new BullAdapter(queue)
-])
+createBullBoard({
+  queues: [
+    new BullAdapter(queue, { readOnlyMode: true }),
+  ],
+  serverAdapter:serverAdapter
+})
+
+const app = express()
+serverAdapter.setBasePath('/')
+
+app.use('/', serverAdapter.getRouter())
 
 const port = config.httpPort
 
