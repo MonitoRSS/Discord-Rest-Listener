dde0a4179fb1896fa5b5d2a566797ce3b0861ed1
gintil <searcher64@gmail.com>
2022-05-27 00:16:05 -0400

    Listen to consumer errors and add initialize call

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ const recordArticleFailure = async (orm: MikroORM, articleMeta: ArticleMeta, err
   await orm.em.nativeInsert(record)
 }
 
-setup().then((initializedData) => {
+setup().then(async (initializedData) => {
   const { orm } = initializedData
   const consumer = new RESTConsumer(config.rabbitmqUri, {
     authHeader: `Bot ${config.token}`,
@@ -34,63 +34,73 @@ setup().then((initializedData) => {
     invalidRequestsThreshold: 1000,
   })
 
-  consumer.on('jobCompleted', async (job, result, jobMetadata) => {
-    log.debug('Job completed', result)
-    logDatadog('info', `Article delivered`, {
-      route: job.route,
-      ...(jobMetadata.startTimestamp && { duration: jobMetadata.endTimestamp - jobMetadata.startTimestamp }),
-      ...(job.meta?.feedURL && { feedURL: job.meta?.feedURL }),
+  try {
+    consumer.on('err', (err) => {
+      const errorMessage = `Consumer error: ${err.message}`
+      log.error(errorMessage)
+      logDatadog('error', errorMessage, {
+        stack: err.stack
+      })
     })
-    // This was a feed article
-    if (!job.meta?.articleID) {
-      return
-    }
-    await recordArticleSuccess(orm, job.meta as ArticleMeta)
-    if (!result.status.toString().startsWith('2')) {
-      await recordArticleFailure(orm, job.meta as ArticleMeta, `Bad status code (${result.status}) | ${JSON.stringify(result.body)}`)
-    }
-  })
 
-  consumer.on('jobError', async (error, job) => {
-    log.error(`Job ${job.id} failed: ${error.message}`)
-    if (!job.meta?.articleID) {
-      return
-    }
-    await recordArticleFailure(orm, job.meta as ArticleMeta, `Job failed: ${error.message}`)
-  })
-
-  /**
-   * Log all the important events that might affect this service's performance
-   */
-  consumer.on('globalBlock', (blockType, durationMs) => {
-    if (blockType === GLOBAL_BLOCK_TYPE.GLOBAL_RATE_LIMIT) {
-    const errorMessage = `Global rate limit hit (retry after ${durationMs}ms)`
-    
-      logDatadog('warn', errorMessage, {
-        durationMs
+    consumer.on('jobCompleted', async (job, result, jobMetadata) => {
+      log.debug('Job completed', result)
+      logDatadog('info', `Article delivered`, {
+        route: job.route,
+        ...(jobMetadata.startTimestamp && { duration: jobMetadata.endTimestamp - jobMetadata.startTimestamp }),
+        ...(job.meta?.feedURL && { feedURL: job.meta?.feedURL }),
       })
-      log.warn(errorMessage)
-    } else if (blockType === GLOBAL_BLOCK_TYPE.CLOUDFLARE_RATE_LIMIT) {
-      const errorMessage = `Cloudflare rate limit hit (retry after ${durationMs}ms)`
+      // This was a feed article
+      if (!job.meta?.articleID) {
+        return
+      }
+      await recordArticleSuccess(orm, job.meta as ArticleMeta)
+      if (!result.status.toString().startsWith('2')) {
+        await recordArticleFailure(orm, job.meta as ArticleMeta, `Bad status code (${result.status}) | ${JSON.stringify(result.body)}`)
+      }
+    })
 
-      logDatadog('warn', errorMessage, {
-        durationMs
-      })
-      log.warn(errorMessage)
-    } else if (blockType === GLOBAL_BLOCK_TYPE.INVALID_REQUEST) {
-      const errorMessage = `Invalid requests threshold reached, delaying all requests by ${durationMs}ms`
+    consumer.on('jobError', async (error, job) => {
+      log.error(`Job ${job.id} error: ${error.message}`)
+      if (!job.meta?.articleID) {
+        return
+      }
+      await recordArticleFailure(orm, job.meta as ArticleMeta, `Job error: ${error.message}`)
+    })
 
-      logDatadog('warn', errorMessage, {
-        durationMs
-      })
-      log.warn(errorMessage)
-    }
-  })
+    /**
+     * Log all the important events that might affect this service's performance
+     */
+    consumer.on('globalBlock', (blockType, durationMs) => {
+      if (blockType === GLOBAL_BLOCK_TYPE.GLOBAL_RATE_LIMIT) {
+      const errorMessage = `Global rate limit hit (retry after ${durationMs}ms)`
+      
+        logDatadog('warn', errorMessage, {
+          durationMs
+        })
+        log.warn(errorMessage)
+      } else if (blockType === GLOBAL_BLOCK_TYPE.CLOUDFLARE_RATE_LIMIT) {
+        const errorMessage = `Cloudflare rate limit hit (retry after ${durationMs}ms)`
 
-  return consumer.initialize()
-}).then(() => {
-  log.info('Ready')
-}).catch(err => {
-  log.error(err)
+        logDatadog('warn', errorMessage, {
+          durationMs
+        })
+        log.warn(errorMessage)
+      } else if (blockType === GLOBAL_BLOCK_TYPE.INVALID_REQUEST) {
+        const errorMessage = `Invalid requests threshold reached, delaying all requests by ${durationMs}ms`
+
+        logDatadog('warn', errorMessage, {
+          durationMs
+        })
+        log.warn(errorMessage)
+      }
+    })
+
+    await consumer.initialize()
+
+    log.info('Ready')
+  } catch (err) {
+    log.error(err)
+  }
 })
 
